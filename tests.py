1878902a6f5d07e7ca973826be79d6c6bb8cc406
wasabii <wasabiizombie@gmail.com>
2025-07-04 03:20:47 -0500

    #0.0.4 Write to files

--- tests.py
+++ tests.py
@@ -1,14 +1,14 @@
-from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
 
 def test():
-    result = get_file_content("calculator", "main.py")
+    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
     print(result)
 
-    result = get_file_content("calculator", "pkg/calculator.py")
+    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
     print(result)
 
-    result = get_file_content("calculator", "/bin/cat")
+    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
     print(result)
 
 
