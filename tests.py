e01be8848507bd6b3732f727c25b19e07945dea4
wasabii <wasabiizombie@gmail.com>
2025-07-04 03:24:10 -0500

    dunno

--- tests.py
+++ tests.py
@@ -1,14 +1,17 @@
-from functions.write_file import write_file
+from functions.run_python import *
 
 
 def test():
-    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+    result = run_python_file("calculator", "main.py")
     print(result)
 
-    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+    result = run_python_file("calculator", "tests.py")
     print(result)
 
-    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+    result = run_python_file("calculator", "../main.py")
+    print(result)
+
+    result = run_python_file("calculator", "nonexistent.py")
     print(result)
 
 
